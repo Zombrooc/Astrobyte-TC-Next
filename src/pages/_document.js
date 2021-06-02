dfbae1738e05c529aa43cb70e753e2ba34a6fa63
Elian Valdez <elian.valdez09@gmail.com>
2021-06-02 13:43:55 -0300

    Add bootstrap

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -34,6 +34,7 @@ export default class MyDocument extends Document {
 
           <meta name="title" content="Astrobyte - Centro de Treinamento" />
           <meta name="description" content="Astrobyte, cursos profissionalizantes que capacitam você a entrar no mercado de trabalho. Comece já sua jornada." />
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossOrigin="anonymous" />
 
           <link
             rel="apple-touch-icon"
@@ -113,6 +114,9 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+
         </body>
       </Html>
     );
